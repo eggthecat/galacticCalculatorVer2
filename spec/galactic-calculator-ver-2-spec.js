edda55f8c31dd6d79235459507a257ff2d41fb61
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-15 16:20:00 -0700

    unknown

---
+++ spec/galactic-calculator-ver-2-spec.js
@@ -0,0 +1,13 @@
+// Notes
+// Primary Objectives
+// √ Take a person age on Earth
+// √ Find out how to collect age for functions
+// √ Apply age to multiple functions to find out various outcomes
+// √ Return age from Mercury @ 24/100 Earth years (fastest journey around sun)
+// √ Return age from Venus @ 62/100 Earth years
+// √ Return age from Mars @ 188/100 Earth years
+// √ Return age from Jupiter @ 1186/100 Earth years (slowest journey around sun)
+// √ For each planet return life left on planet with 'const lifeExpectancy = 100;' result must be either surplus or deficit, not 0.
+// Secondary Obectives
+// Apply orogress to additional objectives mentioned in
+// https://epicenter.epicodus.com/courses/449/code_reviews/1861
