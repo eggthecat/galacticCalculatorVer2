ade54c3599be9f389deae75516b25c34d5339690
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-15 18:48:39 -0700

    add files to push

--- src/galactic-calculator-ver-2.js
+++ src/galactic-calculator-ver-2.js
@@ -1,8 +1,11 @@
 // add 'export' to all functions
 
 // consturctor function
+export class Universal {
 function Universal(birthday) {
   this.birthday = birthday;
   console.log(birthday);
   console.log(this.birthday);
+}
+
 }
--- src/galacticCalculatorVer2.js
+++
@@ -1,6 +0,0 @@
-// add 'export' to all functions
-
-// export function ageExportFunction(age) {
-//   var output = [];
-//   return output;
-// }
