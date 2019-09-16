dcac364b7b1b78472ddd99880de5276ab8007734
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-16 07:15:43 -0700

    Establish function for objectives: age and life expectancy on Earth, Venus, Mercury, Mars, and Jupiter

--- src/galactic-calculator-ver-2.js
+++ src/galactic-calculator-ver-2.js
@@ -1,11 +1,73 @@
-// add 'export' to all functions
 
-// consturctor function
-export class Universal {
-function Universal(birthday) {
-  this.birthday = birthday;
-  console.log(birthday);
-  console.log(this.birthday);
-}
+// class Being {
+  constructor (birthday) {
+    this.birthday = birthday;
+  }
 
+  get earthlingAge() {
+    return this.ageOnEarth();
+  }
+
+  ageOnEarth() {
+    let date = new Date();
+    let dateYear = date.getYear();
+    let age = (dateYear - this.birthDay);
+    return (age);
+  };
+
+  earthYearsLeft() {
+    return  100 - this.earthlingAge;
+  }
+
+  get mercurianAge() {
+    return this.ageOnMercury();
+  }
+
+  ageOnMercury() {
+    let age = this.ageOnEarth / 0.24;
+    return (age);
+  }
+
+  mercuryYearsLeft() {
+    return 100 / 0.24 - this.mercurianAge;
+  }
+
+  get venusianAge() {
+    return this.ageOnVenus();
+  }
+
+  ageOnVenus() {
+    let age = this.ageOnEarth / 0.62
+    return (age);
+  }
+
+  venusYearsLeft() {
+    return 100 / 0.62 - this.venusianAge;
+  }
+
+  get martianAge() {
+    return this.ageOnMars();
+  }
+
+  ageOnMars() {
+    let age = this.ageOnEarth / 1.88;
+    return (age);
+  }
+
+  marsYearsLeft() {
+    return 100 / 1.88 - this.martianAge;
+  }
+
+  get jupiterianAge(){
+    return this.ageOnJupiter();
+  }
+
+  ageOnJupiter() {
+    let age = this.ageOnEarth / 11.86;
+    return (age);
+  }
+
+  jupiterYearsLeft() {
+    return 100/11.86 - this.jupiterAge;
+  }
 }
