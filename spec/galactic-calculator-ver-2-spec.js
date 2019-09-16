7ed15e1c46e6ee86d26853b8dfa7df91f6c97255
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-16 07:14:30 -0700

    Plan initial specs out

--- spec/galactic-calculator-ver-2-spec.js
+++ spec/galactic-calculator-ver-2-spec.js
@@ -11,3 +11,29 @@
 // Secondary Obectives
 // Apply orogress to additional objectives mentioned in
 // https://epicenter.epicodus.com/courses/449/code_reviews/1861
+// import { } from './../src/ .js';
+//
+// describe ('birthday', function ()
+//
+//   it ('should create an instance of a date object with var birthday', function(){
+//     let test = new Date('11, 12, 2001');
+//     expect(test.getMonth()).toEqual(11);
+//   });
+//   it ('should ')
+// })
+
+import { Being } from './galactic-calculator-ver-2.js';
+
+describe('Being', function (){
+  it('should create a being object', function ()
+{
+  const dob = new Being (new birthday(1985, 11, 29));
+  expect(typeof birthday).toEqual(typeof new Being);
+});
+it ('should calculate age in years from a date', function ()
+{
+  const dob = new Being (new birthday(1985, 11, 29));
+  const age = dob.getYear();
+  expect(age).toEqual(35);
+})
+})
