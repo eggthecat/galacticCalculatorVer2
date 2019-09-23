d8f627d14abe663afbde505e552b7f4a5b5b2ebe
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-23 06:21:47 -0700

    Deleted titles for files

--- spec/galactic-calculator-ver-2-spec.js
+++
@@ -1,9 +0,0 @@
-import { Being } from './src/galactic-calculator-ver-2.js';
-
-describe('Being', function(){
-
-  it('should test whether a birthday creates an instance of the Date object', function(){
-    let test = new Date("11, 11, 2011");
-    expect(test.getMonth).toEqual(11);
-  });
-});
