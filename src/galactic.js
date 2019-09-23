ac0058ec375e1e5dd36a9a1393873f8a23d5b70b
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-23 08:11:06 -0700

    Add improved refactoring

--- src/galactic.js
+++ src/galactic.js
@@ -1,69 +1,52 @@
-
 export class Galactic {
   constructor (age) {
     this.age = age;
   }
 
-  // ageOnEarth() {
-  //   let date = new Date();
-  //   let dateYear = date.getYear();
-  //   let age = (dateYear - this.birthDay);
-  //   return (age);
-  // };
-  //
-  // earthYearsLeft() {
-  //   return  100 - this.earthlingAge;
-  // }
-  //
-  // get mercurianAge() {
-  //   return this.ageOnMercury();
-  // }
-  //
-  // ageOnMercury() {
-  //   let age = this.ageOnEarth / 0.24;
-  //   return (age);
-  // }
-  //
-  // mercuryYearsLeft() {
-  //   return 100 / 0.24 - this.mercurianAge;
-  // }
-  //
-  // get venusianAge() {
-  //   return this.ageOnVenus();
-  // }
-  //
-  // ageOnVenus() {
-  //   let age = this.ageOnEarth / 0.62
-  //   return (age);
-  // }
-  //
-  // venusYearsLeft() {
-  //   return 100 / 0.62 - this.venusianAge;
-  // }
-  //
-  // get martianAge() {
-  //   return this.ageOnMars();
-  // }
-  //
-  // ageOnMars() {
-  //   let age = this.ageOnEarth / 1.88;
-  //   return (age);
-  // }
-  //
-  // marsYearsLeft() {
-  //   return 100 / 1.88 - this.martianAge;
-  // }
-  //
-  // get jupiterianAge(){
-  //   return this.ageOnJupiter();
-  // }
-  //
-  // ageOnJupiter() {
-  //   let age = this.ageOnEarth / 11.86;
-  //   return (age);
-  // }
-  //
-  // jupiterYearsLeft() {
-  //   return 100/11.86 - this.jupiterAge;
-  // }
+  ageOnEarth() {
+    let date = new Date();
+    let dateYear = date.getUTCFullYear();
+    let age = DateYear - this.age.getFullYear();
+    return (age);
+  };
+
+  earthYearsLeft() {
+    return  100 - this.ageOnEarth;
+  }
+
+  ageOnMercury() {
+    let age = this.ageOnEarth / 0.24;
+    return (age);
+  }
+
+  mercuryYearsLeft() {
+    return 100 / 0.24 - this.ageOnMercury;
+  }
+
+  ageOnVenus() {
+    let age = this.ageOnEarth / 0.62
+    return (age);
+  }
+
+  venusYearsLeft() {
+    return 100 / 0.62 - this.ageOnVenus;
+  }
+
+  ageOnMars() {
+    let age = this.ageOnEarth / 1.88;
+    return (age);
+  }
+
+  marsYearsLeft() {
+    return 100 / 1.88 - this.ageOnMars;
+  }
+
+  ageOnJupiter() {
+    let age = this.ageOnEarth / 11.86;
+    return (age);
+  }
+
+  jupiterYearsLeft() {
+    return 100/11.86 - this.ageOnJupiter;
+  }
 }
