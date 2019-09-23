fbbd4eb14966b9d8411d92893ad92e7febfc8ab7
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-23 03:13:00 -0700

    Add import tags for bootstrap, bootstrap.min.css, and styles.css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { Being } from './galactic-calculator-ver-2.js';
 import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 $(document).ready(function() {
   $('#user-age-form').submit(function(event) {
