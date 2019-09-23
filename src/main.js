226a07a2f328f0c1985dc664d432654a523f339c
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-22 22:48:39 -0700

    modify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import { Being } from './galactic-calculator-ver-2.js';
+import { Being } from './galactic-calculator-ver-2.js';
 import $ from 'jquery';
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
