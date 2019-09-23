cd3d0a135344e399cf1630155b4a3d6247eaa53f
Dominic Montelongo <montelongodominic@gmail.com>
2019-09-23 06:21:23 -0700

    Rename

---
+++ src/galactic.js
@@ -0,0 +1,69 @@
+
+export class Galactic {
+  constructor (age) {
+    this.age = age;
+  }
+
+  // ageOnEarth() {
+  //   let date = new Date();
+  //   let dateYear = date.getYear();
+  //   let age = (dateYear - this.birthDay);
+  //   return (age);
+  // };
+  //
+  // earthYearsLeft() {
+  //   return  100 - this.earthlingAge;
+  // }
+  //
+  // get mercurianAge() {
+  //   return this.ageOnMercury();
+  // }
+  //
+  // ageOnMercury() {
+  //   let age = this.ageOnEarth / 0.24;
+  //   return (age);
+  // }
+  //
+  // mercuryYearsLeft() {
+  //   return 100 / 0.24 - this.mercurianAge;
+  // }
+  //
+  // get venusianAge() {
+  //   return this.ageOnVenus();
+  // }
+  //
+  // ageOnVenus() {
+  //   let age = this.ageOnEarth / 0.62
+  //   return (age);
+  // }
+  //
+  // venusYearsLeft() {
+  //   return 100 / 0.62 - this.venusianAge;
+  // }
+  //
+  // get martianAge() {
+  //   return this.ageOnMars();
+  // }
+  //
+  // ageOnMars() {
+  //   let age = this.ageOnEarth / 1.88;
+  //   return (age);
+  // }
+  //
+  // marsYearsLeft() {
+  //   return 100 / 1.88 - this.martianAge;
+  // }
+  //
+  // get jupiterianAge(){
+  //   return this.ageOnJupiter();
+  // }
+  //
+  // ageOnJupiter() {
+  //   let age = this.ageOnEarth / 11.86;
+  //   return (age);
+  // }
+  //
+  // jupiterYearsLeft() {
+  //   return 100/11.86 - this.jupiterAge;
+  // }
+}
